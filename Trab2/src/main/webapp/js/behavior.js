c644b3019561cfc93df0b819614a8c6313f7359e
Leonardo Souza <leonardostsouza@gmail.com>
2016-06-20 18:04:37 -0300

    Inclui javascript para pagina html

---
+++ Trab2/src/main/webapp/js/behavior.js
@@ -0,0 +1,135 @@
+/* 
+ * Este codigo faz parte do primeiro trabalho desenvolvido para o curso
+ * EEL 418 - Programacao Avanacada, ministrado no primeiro semestre
+ * de 2016 pelo professor Jorge Lopes de Souza Leao
+ * Autor: Leonardo dos Santos Teixeira de Souza
+ * DRE: 112086681
+ */
+
+function menuBehavior(option){
+    cleanTextFields('all');
+    showSearchResult(false);
+    switch (option){
+        case "entrar":
+            // menu options handling
+            document.getElementById('menuEntrar').style.display='none';
+            document.getElementById('menuSair').style.display='block';
+            document.getElementById('menuCatalogacao').style.display='block';
+            document.getElementById('menuBusca').style.display='none';
+            
+            // page content handling
+            document.getElementById('idDivLogin').style.display='none';
+            document.getElementById('idDivBusca').style.display='block';
+            document.getElementById('idDivCatalogacao').style.display='none';
+            
+            break;
+        case "catalogacao":
+            // menu options handling
+            document.getElementById('menuCatalogacao').style.display='none';
+            document.getElementById('menuBusca').style.display='block';
+            
+            // page content handling
+            document.getElementById('idDivLogin').style.display='none';
+            document.getElementById('idDivBusca').style.display='none';
+            document.getElementById('idDivCatalogacao').style.display='block';
+            break;
+        
+        case "sair":
+            // menu options handling
+            document.getElementById('menuEntrar').style.display='block';
+            document.getElementById('menuSair').style.display='none';
+            document.getElementById('menuCatalogacao').style.display='none';
+            document.getElementById('menuBusca').style.display='none';
+            
+            // page content handling
+            document.getElementById('idDivLogin').style.display='block';
+            document.getElementById('idDivBusca').style.display='none';
+            document.getElementById('idDivCatalogacao').style.display='none';
+            break;
+            
+        case "busca":
+            // menu options handling
+            document.getElementById('menuCatalogacao').style.display='block';
+            document.getElementById('menuBusca').style.display='none';
+            
+            // page content handling
+            document.getElementById('idDivLogin').style.display='none';
+            document.getElementById('idDivBusca').style.display='block';
+            document.getElementById('idDivCatalogacao').style.display='none';
+            break;
+            
+        default:
+            alert("ERRO");
+            break;
+    }
+}
+
+function cleanTextFields(option){
+    switch(option){
+        
+        case 'all':
+            cleanTextFields('login');
+            cleanTextFields('busca');
+            cleanTextFields('catalogacao');
+            break;
+            
+        case 'login':
+            document.getElementById('idUsuario').value = '';
+            document.getElementById('idSenha').value = '';
+            break;
+            
+        case 'busca':
+            //patrionio
+            document.getElementById('idpatrimonio2').value = '';
+            document.getElementById('idcheckpatrimonio').checked = false;
+
+            // titulo
+            document.getElementById('idtitulo2').value = '';
+            document.getElementById('idchecktituloOU').checked = false;
+            document.getElementById('idchecktituloE').checked = false;
+
+            // autoria
+            document.getElementById('idautoria2').value = '';
+            document.getElementById('idcheckautoriaOU').checked = false;
+            document.getElementById('idcheckautoriaE').checked = false;
+
+            // veiculo
+            document.getElementById('idveiculo2').value = '';
+            document.getElementById('idcheckveiculoOU').checked = false;
+            document.getElementById('idcheckveiculoE').checked = false;
+
+            // data da publicacao
+            document.getElementById('iddatapublicacao21').value = '';
+            document.getElementById('iddatapublicacao22').value = '';
+            document.getElementById('idcheckdatapublicacaoOU').checked = false;
+            document.getElementById('idcheckdatapublicacaoE').checked = false;
+
+            // Palavras Chave
+            document.getElementById('idpalchave2').value = '';
+            document.getElementById('idcheckpalchaveOU').checked = false;
+            document.getElementById('idcheckpalchaveE').checked = false;
+            break;
+            
+        case 'catalogacao':
+            document.getElementById('idpatrimonio3').value = '';
+            document.getElementById('idtitulo3').value = '';
+            document.getElementById('idautoria3').value = '';
+            document.getElementById('idveiculo3').value = '';
+            document.getElementById('iddatapublicacao3').value = '';
+            document.getElementById('idpalchave3').value = '';
+            break;
+         
+        default:
+            alert('Erro de parametro em cleanTextFields')
+            
+            
+    }
+}
+
+function showSearchResult(show){
+    var displayType = 'none';
+    if (show === 'true'){
+        displayType = 'block';
+    }
+    document.getElementById('idDivResultados').style.display = displayType;
+}
